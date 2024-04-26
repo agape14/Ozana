5554b18e4d08d2c5278c74d31a427ef66ef67894
agape14 <adelacruzcarlos@gmail.com>
2024-04-26 10:18:55 -0500

    Initial commit

---
+++ footer.php
@@ -0,0 +1,126 @@
+    <!-- footer -->
+    <footer class="footer secondery-bg">
+        <div class="footer-top pt-110 pb-70">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <!-- footer widget -->
+                        <div class="footer-widget">
+                            <div class="widget-title">
+                                <h3>Address</h3>
+                            </div>
+                            <p>666 Road Broklyn Street, 88 New York, USA</p>
+                        </div>
+                        <!-- End of footer widget -->
+                    </div>
+
+                    <div class="col-md-4">
+                        <!-- footer widget -->
+                        <div class="footer-widget text-center">
+                            <div class="widget-title">
+                                <h3>Follow</h3>
+                            </div>
+                            <div class="footer-social-area">
+                                <ul class="social-list mb--0 list-unstyled">
+                                    <li>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="fa fa-facebook"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="fa fa-pinterest"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="#"><i class="fa fa-youtube"></i></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- End of footer widget -->
+                    </div>
+
+                    <div class="col-md-4">
+                        <!-- footer widget -->
+                        <div class="footer-widget text-right">
+                            <div class="widget-title">
+                                <h3>Contact</h3>
+                            </div>
+                            <div class="footer-contact-info">
+                                <a href="0008888999">000 8888 999</a>
+                                <a href="needhelp@mineralo.com">needhelp@mineralo.com</a>
+                            </div>
+                        </div>
+                        <!-- End of footer widget -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom text-center">
+            <div class="footer-bottom-text">
+                <p>© Copyright 2018 by <a href="#">Layerdrops.com</a></p>
+            </div>
+        </div>
+    </footer>
+    <!-- End of footer -->
+
+    <!-- back to top -->
+    <a class="robbu-whatsapp-button" target="_blank" href="https://api.whatsapp.com/send?phone=51981525451&text=Hola, te escribo luego de revisar tu página web.">
+        <div class="rwb-tooltip">Contactanos por Whatsapp</div>
+        <img src="assets/img/whatsapp-icon.svg">
+    </a>
+
+    <div class="back-to-top">
+        <a href="#" class="back-to-top-icon"><i class="fa fa-chevron-up"></i></a>
+    </div>
+
+    <!-- back to top -->
+
+
+    <!-- JS Files -->
+   <!-- ==== JQuery 3.3.1 js file==== -->
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+
+    <!-- ==== Bootstrap js file==== -->
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+    <!-- ==== JQuery Waypoint js file==== -->
+    <script src="assets/plugins/waypoints/jquery.waypoints.min.js"></script>
+
+    <!-- ==== Parsley js file==== -->
+    <script src="assets/plugins/parsley/parsley.min.js"></script>
+
+    <!-- ==== Ratina js file==== -->
+    <script src="assets/plugins/retinajs/retina.min.js"></script>
+
+    <!-- ==== parallax js==== -->
+    <script src="assets/plugins/parallax/parallax.js"></script>
+
+    <!-- ==== Owl Carousel js file==== -->
+    <script src="assets/plugins/owl-carousel/owl.carousel.min.js"></script>
+
+    <!-- ==== Menu  js file==== -->
+    <script src="assets/js/menu.min.js"></script>
+
+    <!-- ===video popup=== -->
+    <script src="assets/plugins/Magnific-Popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- ====Counter js file=== -->
+    <script src="assets/plugins/waypoints/jquery.counterup.min.js"></script>
+
+    <!-- cammera js -->
+    <script src="assets/plugins/camera/camera.min.js"></script>
+
+    <!-- easing js -->
+    <script src="assets/js/easing.js"></script>
+
+     <!--==== google map api key ====-->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB2D8wrWMY3XZnuHO6C31uq90JiuaFzGws"></script>
+
+    <!-- ==== Script js file==== -->
+    <script src="assets/js/scripts.js"></script>
+
+    <!-- ==== Custom js file==== -->
+    <script src="assets/js/custom.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Document Title -->
+    <title>OZANA</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/ico" href="favicon.ico">
+
+    <!-- CSS Files -->
+    <!--==== Google Fonts ====-->
+    <link href="https://fonts.googleapis.com/css?family=Exo+2:400,500,600,700%7CSchoolbell" rel="stylesheet">
+
+    <!--==== Bootstrap css file ====-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <!--==== Font-Awesome css file ====-->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+    <!-- Owl Carusel css file -->
+    <link rel="stylesheet" href="assets/plugins/owl-carousel/owl.carousel.min.css">
+
+    <!-- camera css -->
+    <link rel="stylesheet" href="assets/plugins/camera/camera.min.css">
+
+    <!-- ====video poppu css==== -->
+    <link rel="stylesheet" href="assets/plugins/Magnific-Popup/magnific-popup.css">
+
+    <!--==== Style css file ====-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!--==== Responsive css file ====-->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+    <!--==== Custom css file ====-->
+    <link rel="stylesheet" href="assets/css/custom.css">
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div class="preLoader">
+        <div class="preload-img">
+            <img src='assets/img/preloader.gif' alt="" class="pre-img">
+        </div>
+    </div>
+    <!-- End Of Preloader -->
+
+    <!-- Main header -->
+    <header class="header">
+        <!-- Start Header Navbar-->
+        <div class="main-header">
+            <div class="main-menu-wrap">
+                <div class="container-fluide">
+                    <div class="row align-items-center">
+                    <div class="logo-container col-xl-3 col-lg-3 col-md-4 col-sm-4 col-6">
+                        <!-- Logo -->
+                        <div class="logo">
+                            <a href="index.html">
+                                <img src="assets/img/logo-ozanav5.png" data-rjs="2" alt="ozana">
+                            </a>
+                        </div>
+                        <!-- End of Logo -->
+                    </div>
+                        <div class="col-xl-6 col-lg-6 col-md-4 col-sm-4 col-6 menu-button">
+                            <div class="menu--inner-area clearfix">
+                                <div class="menu-wraper">
+                                    <nav>
+                                        <!-- Header-menu -->
+                                        <div class="header-menu dosis">
+                                            <ul>
+                                                <li class="active"><a href="#">Inicio <img src="assets/img/line2.png" alt=""></a>
+                                                    <ul>
+                                                        <li class="active"><a href="index.html">Inicio v1</a></li>
+                                                        <li><a href="index-2.html">Inicio v2</a></li>
+                                                        <li><a href="index-3.html">Inicio v3</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li><a href="about.html">Nosotros <img src="assets/img/line2.png" alt=""></a></li>
+                                                <li><a href="#">Servicios<img src="assets/img/line2.png" alt=""></a>
+                                                    <ul>
+                                                        <li><a href="service.html">Servicios</a></li>
+                                                        <li><a href="service-details.html">Detalle Servicio</a></li>
+                                                    </ul>
+                                                </li>
+                                                <!--<li>
+                                                    <a href="#">Blog <img src="assets/img/line2.png" alt=""></a>
+                                                    <ul>
+                                                        <li><a href="blog.html">Blog Layout</a></li>
+                                                        <li><a href="blog-details.html">Blog Details</a></li>
+                                                    </ul>
+                                                </li>-->
+                                                <li><a href="contact.html">Contáctanos<img src="assets/img/line2.png" alt=""></a></li>
+                                            </ul>
+                                        </div>
+                                        <!-- End of Header-menu -->
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-4 col-sm-4">
+                            <div class="urgent-call text-right">
+                                <span><i class="fa fa-phone fa-lg" style="color: #008195;" aria-hidden="true"></i></span><a href="#">+51 947 758 615</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Header Navbar-->
+    </header>
+    <!-- End of Main header -->
